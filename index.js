ce842d27bd378eedc37ef4be6e46a514fefad05a
dheerendra kumar <dheerendra16kumar@gmail.com>
2025-07-12 23:52:09 +0530

    add origin in env

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-// app.use(cors({origin: "https://blog-frontend-eta-silk.vercel.app"})
-app.use(cors())
+app.use(cors({origin: process.env.ORIGIN}));
 app.use("/api/v1/user/", userRouter);
 app.use("/api/v1/", blogRouter);
 // app.use("/api/v1/blog", commentRouter)
